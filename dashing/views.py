a489a959c662f06972428152ce9cc7d7e8ea99a5
M. de Verteuil <onceuponajooks@gmail.com>
2014-10-08 13:56:32 -0400

    Provide an overrideable way to set user permissions for dashboard

--- dashing/views.py
+++ dashing/views.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf-8 -*-
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
-from django.contrib.admin.views.decorators import staff_member_required
+
+from .settings import user_test
 
 class Dashboard(TemplateView):
     template_name = 'dashing/dashboard.html'
-    
-    @method_decorator(staff_member_required)
+
+    @method_decorator(user_test)
     def dispatch(self, *args, **kwargs):
         return super(Dashboard, self).dispatch(*args, **kwargs)
