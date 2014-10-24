41c654547d2db6281323025bc41478695a959f05
M. de Verteuil <onceuponajooks@gmail.com>
2014-10-23 23:19:20 -0400

    PEP8 fixes

--- dashing/views.py
+++ dashing/views.py
@@ -3,9 +3,10 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
 
+
 class Dashboard(TemplateView):
     template_name = 'dashing/dashboard.html'
-    
+
     @method_decorator(staff_member_required)
     def dispatch(self, *args, **kwargs):
         return super(Dashboard, self).dispatch(*args, **kwargs)
