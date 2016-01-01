d494f7f199297100e08e44b9eed8528291f5c16d
Mauricio Reyes <mreyes@talpor.com>
2016-01-01 17:31:03 -0430

    update test_app for django >= 1.8

--- test/test_app/test_app/urls.py
+++ test/test_app/test_app/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from dashing.utils import router
 
 from .views import MultipleDashboards
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^dashboard/', include(router.urls)),
     url(r'^multiple_dashboards/$', MultipleDashboards.as_view()),
-)
+]
