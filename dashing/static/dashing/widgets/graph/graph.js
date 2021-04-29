387072aa46b9e439fc7b3f58f48402af047fb609
wickeym <wickeym@gmail.com>
2021-04-29 10:20:12 -0400

    UPDATE: Add hoverFormatter to graph

--- dashing/static/dashing/widgets/graph/graph.js
+++ dashing/static/dashing/widgets/graph/graph.js
@@ -2,7 +2,7 @@
 
 Dashing.widgets.Graph = function (dashboard) {
     var self = this;
-    self.__init__ =  Dashing.utils.widgetInit(dashboard, 'graph', {
+    self.__init__ = Dashing.utils.widgetInit(dashboard, 'graph', {
         require: ['d3', 'rickshaw']
     });
     self.row = 1;
@@ -11,7 +11,7 @@ Dashing.widgets.Graph = function (dashboard) {
     self.getWidget = function () {
         return this.__widget__;
     };
-    self.getData = function () {};
+    self.getData = function () { };
     self.interval = 3000;
 };
 
@@ -19,8 +19,16 @@ rivets.binders['dashing-graph'] = function binder(el, data) {
     if (!data) return;
     if (!window.Rickshaw) {
         $(document).on('libs/rickshaw/loaded',
-                       binder.bind(this, el, data));
+            binder.bind(this, el, data));
         return;
+    }
+
+    var defaultHoverFormatter = function (series, x, y) {
+        var date = '<span class="date">' + new Date(x * 1000).toUTCString() + '</span>';
+        var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
+        //var content = swatch + series.name + ": " + parseInt(y) + '<br>' + date;
+        var content = swatch + "count" + ": " + parseInt(y) + '<br>' + date;
+        return content;
     }
 
     var container = el.parentNode, id, graph, xAxis, yAxis,
@@ -29,7 +37,10 @@ rivets.binders['dashing-graph'] = function binder(el, data) {
         afterRender = this.model.afterRender || data.afterRender,
         xFormat = this.model.xFormat || data.xFormat,
         yFormat = this.model.yFormat || data.yFormat,
-        properties = this.model.properties || {};
+        properties = this.model.properties || {},
+        yAxisType = this.model.yAxisType || 1, //Display Axis.Y default format Number
+        xAxisType = this.model.xAxisType || 1, //Display Axis.X default format Number
+        hoverFormatter = this.model.hoverFormatter || defaultHoverFormatter;
 
     if (!$(container).is(':visible')) return;
     if (beforeRender) beforeRender();
@@ -52,14 +63,35 @@ rivets.binders['dashing-graph'] = function binder(el, data) {
     graph.configure(properties);
     graph.render();
 
-    xAxis = new Rickshaw.Graph.Axis.X({
+    data.hoverFormatter = hoverFormatter
+
+    var hoverDetail = new Rickshaw.Graph.HoverDetail({
         graph: graph,
-        tickFormat: xFormat || Rickshaw.Fixtures.Number.formatKMBT
+        formatter: data.hoverFormatter
+        //xFormatter: function(x) { return x + "seconds" },
+        //yFormatter: function(y) { return Math.floor(y) + " percent" }
     });
-    yAxis = new Rickshaw.Graph.Axis.Y({
-        graph: graph,
-        tickFormat: yFormat || Rickshaw.Fixtures.Number.formatKMBT
-    });
+    //var time = new Rickshaw.Fixtures.Time();
+    //var days = time.unit('day');
+    //xAxis = new Rickshaw.Graph.Axis.Time( { graph: graph, timeUnit: days } );
+    if (xAxisType) {
+        xAxis = new Rickshaw.Graph.Axis.X({
+            graph: graph,
+            tickFormat: xFormat || Rickshaw.Fixtures.Number.formatKMBT
+        });
+    } else {
+        xAxis = new Rickshaw.Graph.Axis.Time({ graph: graph });
+    }
+
+    if (yAxisType) {
+        yAxis = new Rickshaw.Graph.Axis.Y({
+            graph: graph,
+            tickFormat: yFormat || Rickshaw.Fixtures.Number.formatKMBT
+        });
+    } else {
+        yAxis = new Rickshaw.Graph.Axis.Time({ graph: graph });
+    }
+
     xAxis.render();
     yAxis.render();
     if (afterRender) afterRender();
