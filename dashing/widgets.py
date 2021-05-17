83d80ce01f567c6b698090dbb117ac6abef8e1cf
wickeym <wickeym@gmail.com>
2021-05-17 07:46:28 -0400

    UPDATE: Add support for setting icon in widgets python file

--- dashing/widgets.py
+++ dashing/widgets.py
@@ -55,6 +55,7 @@ class NumberWidget(Widget):
     updated_at = ''
     detail = ''
     value = ''
+    icon = ''
 
     def get_title(self):
         return self.title
@@ -71,6 +72,9 @@ class NumberWidget(Widget):
     def get_value(self):
         return self.value
 
+    def get_icon(self):
+        return self.icon
+
     def get_context(self):
         return {
             'title': self.get_title(),
@@ -78,6 +82,7 @@ class NumberWidget(Widget):
             'updatedAt': self.get_updated_at(),
             'detail': self.get_detail(),
             'value': self.get_value(),
+            'icon': self.get_icon(),
         }
 
 
